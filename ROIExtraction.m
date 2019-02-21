8d0873ae8b108c24ef6593a5d7c1567237f77381
zhaobaotian <zhaobaotian0220@foxmail.com>
2019-02-21 12:29:55 +0800

    Update ROIExtraction.m

--- ROIExtraction.m
+++ ROIExtraction.m
@@ -1,14 +1,9 @@
-im = dicomread('IMG-0002-00001.dcm');
-dicomdisp(im)
-h = imfreehand
-
-
-dicomdisp('IMG-0002-00001.dcm')
-
+% Import raw data and masks sepertately and cao
 
 Data = niftiread('6 t2_fl2d_tra_2.5mm_BW40.nii');
 mask = niftiread('6 t2_fl2d_tra_2.5mm_BW40-label.nii');
 Contrast = niftiread('6 t2_fl2d_tra_2.5mm_BW40-label_1.nii');
+BackgroundNoise
 
 
 Results = single(Data) .* single(mask); 
@@ -18,4 +13,9 @@ ResultsContrast = single(Data) .* single(Contrast);
 meanSTN = sum(sum(sum(Results)))/sum(sum(sum(mask)))
 meanContrast = sum(sum(sum(ResultsContrast)))/sum(sum(sum(Contrast)))
 
-imtool(squeeze(Data(:,:,19)))
+imtool(squeeze(Data(:,:,30)))
+
+% Another way to calculate the mean value
+Index = find(ResultsContrast);
+ResultsContrastNew = ResultsContrast(Index);
+mean(ResultsContrastNew)
